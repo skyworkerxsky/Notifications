5268e680195c678767a61be221f4a5c01f8940fe
skyworkerxsky <pwuser@yandex.ru>
2020-01-11 15:57:16 +0500

    Подключение центра уведомлений

--- NotificationsDemo/NotificationsDemo/AppDelegate.swift
+++ NotificationsDemo/NotificationsDemo/AppDelegate.swift
@@ -7,31 +7,51 @@
 //
 
 import UIKit
+import NotificationCenter
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    let notificationCenter = UNUserNotificationCenter.current()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        requestAutorization()
+        
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    // запрос на отправку уведомлений
+    func requestAutorization() {
+        notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            print("Permission granted: \(granted)")
+            
+            guard granted else { return }
+            self.getNotificationSetting()
+        }
+    }
+    
+    // запрос настроек из центра уведомлений
+    func getNotificationSetting() {
+        notificationCenter.getNotificationSettings { (settings) in
+            print("Notification settings: \(settings)")
+        }
+    }
+    
+    
 }
 
